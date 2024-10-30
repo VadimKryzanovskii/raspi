b5b1922198fd1626e5a2b64602bb74bca7ab87da
Vadim <vadimkryzanovskii@gmail.com>
2024-10-30 19:46:55 +0300

    test controler

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print("Hello Vadim its OK")
+for event in gamepad.read_loop():
+    if event.type == ecodes.EV_ABS:
+        if event.code == ecodes.ABS_X:
+            print("Левый стик X:", event.value)
