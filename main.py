bd40e1f3b16a08d237c18ec8590cec1b93003f8e
Vadim <vadimkryzanovskii@gmail.com>
2024-10-31 13:36:16 +0300

    Данные с 4 стиков

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 for event in gamepad.read_loop():
     if event.type == ecodes.EV_ABS:
-        if event.code == ecodes.ABS_X:
-            print("Левый стик X:", event.value)
+        absevent = categorize(event)
+        if event.code == ecodes.ABS_X:  # Событие оси X левого стика
+            print(f'Левый стик (ось X): {event.value}')
+        elif event.code == ecodes.ABS_Y:  # Событие оси Y левого стика
+            print(f'Левый стик (ось Y): {event.value}')
+        elif event.code == ecodes.ABS_RX:  # Событие оси X правого стика
+            print(f'Правый стик (ось X): {event.value}')
+        elif event.code == ecodes.ABS_RY:  # Событие оси Y правого стика
+            print(f'Правый стик (ось Y): {event.value}')
