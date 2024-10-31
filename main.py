02ca55fd915f02587cf742c989ebd53f91a01286
Vadim <vadimkryzanovskii@gmail.com>
2024-10-31 14:31:41 +0300

    Работа с сервоприводом

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
-for event in gamepad.read_loop():
-    if event.type == ecodes.EV_ABS:
-        absevent = categorize(event)
-        if event.code == ecodes.ABS_X:  # Событие оси X левого стика
-            print(f'Левый стик (ось X): {event.value}')
-        elif event.code == ecodes.ABS_Y:  # Событие оси Y левого стика
-            print(f'Левый стик (ось Y): {event.value}')
-        elif event.code == ecodes.ABS_RX:  # Событие оси X правого стика
-            print(f'Правый стик (ось X): {event.value}')
-        elif event.code == ecodes.ABS_RY:  # Событие оси Y правого стика
-            print(f'Правый стик (ось Y): {event.value}')
+import RPi.GPIO as GPIO
+import time
+
+# Настройка GPIO
+GPIO.setmode(GPIO.BCM)
+servo_pin = 17
+GPIO.setup(servo_pin, GPIO.OUT)
+
+# Установка сигнала на выходе
+pwm = GPIO.PWM(servo_pin, 50)  # Частота 50 Гц для сервопривода
+pwm.start(0)  # Начальное значение
+
+# Функция для поворота сервопривода
+def set_angle(angle):
+    duty = 2 + (angle / 18)  # Перевод угла в значение скважности (duty cycle)
+    pwm.ChangeDutyCycle(duty)
+    time.sleep(0.5)
+    pwm.ChangeDutyCycle(0)  # Остановим сигнал для предотвращения шума
+
+# Пример использования
+try:
+    set_angle(0)   # Позиция 0 градусов
+    time.sleep(1)
+    set_angle(90)  # Позиция 90 градусов
+    time.sleep(1)
+    set_angle(180) # Позиция 180 градусов
+    time.sleep(1)
+finally:
+    pwm.stop()
+    GPIO.cleanup()
